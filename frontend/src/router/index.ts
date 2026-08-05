6f9d8cb0261d2ce5a76f62e5de8aa2ca63491a1c
hyeonjin Hwang <104902715+hyeonjin6530@users.noreply.github.com>
2026-08-05 11:03:36 +0900

    [FE] feat: 자산 관리 이체내역 상세페이지 구현 (#98)
    * feat: 자산 관리 이체내역 상세페이지 퍼블리싱
    
    * feat: 메모 수정 기능 구현

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -63,7 +63,7 @@ const routes: RouteRecordRaw[] = [
     name: 'AssetDetail',
     component: () => import('@/views/assets/AssetDetailView.vue'),
     props: true,
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, headerTitle: '자산관리' },
   },
 
   // 타임캡슐
