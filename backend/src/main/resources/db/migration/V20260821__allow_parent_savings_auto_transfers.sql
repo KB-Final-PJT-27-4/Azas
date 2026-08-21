cfa46fce841d4a025be30ac70ef5da4c0a887732
junsoo0719 <junsoo0719@gmail.com>
2026-08-21 23:15:47 +0900

    chore: 중복 RDS 마이그레이션 제거

--- backend/src/main/resources/db/migration/V20260821__allow_parent_savings_auto_transfers.sql
+++
@@ -1,4 +0,0 @@
--- 부모 또는 자녀 적금으로 Mock 자동이체를 등록할 수 있도록 목표와 자녀 연결을 선택값으로 전환한다.
-ALTER TABLE auto_transfer_schedule
-    MODIFY COLUMN child_id BIGINT UNSIGNED NULL COMMENT '자동이체 대상 자녀(자녀 적금인 경우)',
-    MODIFY COLUMN financial_goal_id BIGINT UNSIGNED NULL COMMENT '연결 금융 목표 ID(있는 경우)';
